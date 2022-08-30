93400f10f5cacf547b403d3a0371a61e0c230a3a
Jordan Addis <jordanaddis@Jordans-MBP.lan>
2022-08-30 12:15:34 +0100

    Validate on submit (prevent send if invalid) && fixed error with getElementById(removed #)

--- validate-form.js
+++ validate-form.js
@@ -1,19 +1,27 @@
-const nameInput = document.getElementById('#input-name');
-const emailInput = document.getElementById('#input-email');
-const messageInput = document.getElementById('#input-message');
+const nameInput = document.getElementById('input-name');
+const emailInput = document.getElementById('input-email');
+const messageInput = document.getElementById('input-message');
+const submitButton = document.querySelector('button[type="submit"]');
 
 //Validate name - Check not empty
 const validateName = () => {
-    return nameInput.value.trim() !== "";
+    return nameInput.value.trim();
 }
 
 //Validate email - Check right format (regex) && not empty
 const validateEmail = () => {
     let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}])|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(emailInput.value.trim());
+    return emailInput.value.trim() && regex.test(emailInput.value);
 }
 
 //Validate message - Check not empty
 const validateMessage = () => {
     return messageInput.value.trim() !== "";
 }
+
+//Validate on submit button press - prevent submit if not valid
+submitButton.addEventListener('click', (e) => {
+    if (!validateName() || !validateEmail() || !validateMessage()) {
+        e.preventDefault()
+    }
+})
