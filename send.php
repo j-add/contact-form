e73f75de04e2b13390e81266877b9bb6c2a80beb
Jordan Addis <jordanaddis@Jordans-MBP.lan>
2022-08-30 13:24:30 +0100

    Sanitise and validate data, with feedback

--- send.php
+++ send.php
@@ -5,11 +5,36 @@ if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    //Sanitise
+    //Sanitise - Capitalise name, convert special chars to HTML entities, and remove illegal characters from email
+    $cleanedName = htmlspecialchars(ucwords(trim($name)));
+    $cleanedEmail = filter_var(trim($email), FILTER_SANITIZE_EMAIL);
+    $cleanedMessage = htmlspecialchars(trim($message));
 
-    //Validate
+    //Validate - Check inputs are not empty and email is valid
+    //If all fields are valid, show the message, else show errors
+    if (!empty($cleanedName)
+        && filter_var($cleanedEmail, FILTER_VALIDATE_EMAIL)
+        && !empty($cleanedMessage)) {
+        echo nl2br("Success!\nYour message:\n");
+        echo nl2br("Name: $cleanedMessage\nEmail: $cleanedEmail\nMessage: $cleanedMessage");
+        echo "<br><a href='index.html'>Back to form</a>";
+    } else {
+        echo "Uh oh, looks like something's wrong with your data";
+        echo "<ul>";
 
-    //If all fields are valid, do the thing, else display error
+        if (!empty($cleanedName)) {
+            echo "<li>You did not enter a valid name</li>";
+        }
+
+        if (!filter_var($cleanedEmail, FILTER_VALIDATE_EMAIL)) {
+            echo "<li>You did not enter a valid email</li>";
+        }
+
+        if (empty($cleanedMessage)) {
+            echo "<li>You did not enter a message</li>";
+        }
+        echo "</ul><br><a href='index.html'>Back to form</a>";
+    }
 
 } else {
     header('Location: index.html');
