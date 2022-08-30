ea35a8faf589310f74d94f6e2c5647ff0533c060
Jordan Addis <jordanaddis@Jordans-MBP.lan>
2022-08-30 17:14:20 +0100

    Added anti-spam hidden field to form

--- send.php
+++ send.php
@@ -6,12 +6,11 @@
     <title>Contact Us</title>
     <link href="normalize.css" type="text/css" rel="stylesheet">
     <link href="styles.css" type="text/css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/7c6af540e7.js" crossorigin="anonymous"></script>
 </head>
 
 <?php
 //Check if user has come from form and submitted data, else kick back to form
-if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])) {
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message']) && empty($_POST['url'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
@@ -21,13 +20,36 @@ if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'
     $cleanedEmail = filter_var(trim($email), FILTER_SANITIZE_EMAIL);
     $cleanedMessage = htmlspecialchars(trim($message));
 
-    //Validate - Check inputs are not empty and email is valid
+
+    //Validate - Check inputs are not empty and email is valid (using built-in functions)
     //If all fields are valid, show the message, else show errors
+
     echo "<section class='message'>";
 
     if (!empty($cleanedName)
         && filter_var($cleanedEmail, FILTER_VALIDATE_EMAIL)
         && !empty($cleanedMessage)) {
+
+        //Mail Info
+//        $to = "yourEmail@email.com";
+//        $subject = "Message from contact form";
+//        $htmlMsg = "<p style='overflow-wrap: break-word; hyphens: auto;'>"
+//            . "<strong>Name: </strong>$cleanedName<br>"
+//            . "<strong>Email: </strong>$cleanedEmail<br>"
+//            . "<strong>Message: </strong><span style='display: block; margin: 5px 0;'>$cleanedMessage</span>"
+//            . "</p>";
+//        $headers[] = 'MIME-Version: 1.0';
+//        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+//        $headers[] = 'From: WebForm <webform@example.com>';
+//        $headers[] = 'Reply-To: ' . $cleanedEmail;
+//        $headers[] = 'X-Mailer: PHP/' . phpversion();
+//
+//        $success = mail($to, $subject, $message, implode("\r\n", $headers));
+//        if (!$success) {
+//            $errorMessage = error_get_last()['message'];
+//            console.log($errorMessage);
+//        }
+
         echo "<h1>Success!</h1>";
         echo "<p>Your message:</p>";
         echo "<p>"
